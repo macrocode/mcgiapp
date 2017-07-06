20887cdc915ab401ade63cb57d9cbdcdf2f336f0
William M. Ragrag <wmragrag@prescribe.com>
2017-07-06 19:13:15 +0800

    add mcgi exporter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,7 @@ export interface PageInterface {
 @Component({
   templateUrl: 'app.template.html'
 })
-export class ConferenceApp {
+export class MCGIApp {
   // the root nav is a child of the root app component
   // @ViewChild(Nav) gets a reference to the app's root nav
   @ViewChild(Nav) nav: Nav;
